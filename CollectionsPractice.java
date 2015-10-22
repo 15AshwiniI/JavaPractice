312be554dd6a3f97ff8d38e70941ddf1c538e050
Ashwini Iyer <bookworm.iyer@gmail.com>
2015-10-22 18:31:50 -0400

    Working on Collections

--- CollectionsPractice.java
+++ CollectionsPractice.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.*;
+
 public class CollectionsPractice {
 
 	public CollectionsPractice() {
@@ -6,7 +7,7 @@ public class CollectionsPractice {
 	}
 
 	public void ArListPractice() {
-		/*
+
 		ArrayList<String> alist = new ArrayList<String>();
 		alist.add("hi");
 		alist.add("bye");
@@ -16,7 +17,8 @@ public class CollectionsPractice {
 		alist.remove("bye");for(int i = 0; i < alist.size(); i++) {
 			System.out.println(alist.get(i));
 		}
-		*/
+		
+		/*
 		ArrayList tasks = new ArrayList();
 		tasks.add("Eat");
 		tasks.add("Sleep");
@@ -25,10 +27,18 @@ public class CollectionsPractice {
 		for (Object task: tasks) {
 			System.out.println(task);
 		}
+		*/
+	}
+
+	public void SetPractice() {
+		List<String> nameList = Arrays.asList("Alan", "Ada", "Alan");
+		Set<String> nameSet = new HashSet<>(nameList);
+		System.out.println("nameSet: " + nameSet);
 	}
 
 	public static void main(String[] args) {
 		CollectionsPractice cp = new CollectionsPractice();
 		cp.ArListPractice();
+		cp.SetPractice();
 	}
 }
